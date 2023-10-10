25751efed36b4d4e3923fe8afe336703f96e3477
Abdul4real <137358627+Abdul4real@users.noreply.github.com>
2023-10-10 12:14:40 -0400

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,4 @@
+// Alert: For production environment never expose your connection string AND secret keys.
+module.exports = {
+    "ATLASDB" : "mongodb+srv://dbuser:Qp2cioBSDgqjAHkF@cluster0.skexftm.mongodb.net/products?retryWrites=true&w=majority"
+}
